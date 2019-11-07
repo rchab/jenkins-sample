6dfd6fc8e2156a4ae00007150824958f10615329
Chaban, Rostyslav <rostyslav.chaban@partner.cgm.com>
2019-11-07 10:20:10 +0200

    Initial commit

---
+++ src/main/java/com/rchab/jenkins/App.java
@@ -0,0 +1,15 @@
+package com.rchab.jenkins;
+
+/**
+ * Copyright c) 2018 CompuGROUP Software GmbH
+ * This software is the confidential and proprietary information of
+ * CompuGROUP Software GmbH. You shall not disclose such confidential
+ * information and shall use it only in accordance with the terms of
+ * the liense agreement you entered into with CompuGROUP Software GmbH.
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from Mr. Jenkins");
+    }
+}
